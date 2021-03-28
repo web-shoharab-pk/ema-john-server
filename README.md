369a0bc2a6aa912e26d70e6d096d57791edeeee2
md shoharab pk <shoharabpk.2019@gmail.com>
2021-03-28 22:46:14 +0600

    server added

---
+++ README.md
@@ -0,0 +1,2 @@
+# ema-john-server
+# ema-john-server
---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qkzne.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const cors = require('cors')
+const port = 4000
+
+app.use(cors())
+
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  console.log(err);
+  const productsCollection = client.db("emasohnstore").collection("emajohnproduct");
+  const ordersCollection = client.db("emasohnstore").collection("emajohnproductorders");
+
+  app.post('/addProduct', (req, res) => {
+    const product = req.body;
+    console.log(product);
+    productsCollection.insertOne(product)
+      .then(result => {
+        res.send(result.insertedCount)
+      })
+  })
+
+  app.get('/products', (req, res) => {
+    productsCollection.find({})
+      .toArray((err, documents) => {
+        res.send(documents)
+      })
+  })
+  app.get('/product/:key', (req, res) => {
+    productsCollection.find({ key: req.params.key })
+      .toArray((err, documents) => {
+        res.send(documents[0])
+      })
+  })
+
+  app.post('/productsByKeys', (req, res) => {
+    const productsKeys = req.body;
+    productsCollection.find({ key: { $in: productsKeys } })
+      .toArray((err, documents) => {
+        res.send(documents)
+      })
+  })
+
+  app.post('/addOrder', (req, res) => {
+    const order = req.body;
+    console.log(order);
+    ordersCollection.insertOne(order)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
+  })
+
+  // perform actions on the collection object
+});
+
+
+
+app.listen(port)
